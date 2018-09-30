5c1cbf3171ca12dfd55f8d2f54c9ae07c53e644c
ThorayaShaheen <shaheent@tcd.ie>
2018-09-30 23:29:50 +0100

    added lca test function, does not pass test yet

--- LCA1/LCATest.java
+++ LCA1/LCATest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 
 public class LCATest {
 
-	  @Test
+	    @Test
 	    public void insertTest() {
-				Node root = new Node(3);
+				  Node root = new Node(3);
 	        root = LCA.insert(root, root.data);
 
 	        	LCA.insert(root, 4);
@@ -27,4 +27,21 @@ public class LCATest {
 	        assertTrue(LCA.find(root, 9));
 	        assertTrue(LCA.find(root, 7));
 	    }
+			@Test
+ 	    public void lcaTest() {
+ 	    		Node root = new Node(6);
+ 	       // root = LCA.insert(root, root.data);
+
+ 	        	LCA.insert(root, 7);
+ 	        	LCA.insert(root, 9);
+ 	        	LCA.insert(root, 14);
+ 	        	LCA.insert(root, 8);
+ 	        	LCA.insert(root, 1);
+ 	        	LCA.insert(root, 25);
+ 	        	LCA.insert(root, 70);
+
+ 	        	root.data = 9;
+ 	        	Node result = LCA.lca(root, 9, 25);
+ 	        	assertEquals(root.data,result);
+ 	    }
 }
