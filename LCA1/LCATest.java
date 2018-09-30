5641fa2ff08c01b314030274547f8fd4523d1cc4
ThorayaShaheen <shaheent@tcd.ie>
2018-09-30 22:36:58 +0100

    some fixes

--- LCA1/LCATest.java
+++ LCA1/LCATest.java
@@ -6,19 +6,25 @@ public class LCATest {
 
 	  @Test
 	    public void insertTest() {
-	        Node root = new Node(3);
-	        LCA in = new LCA();
-	        root = in.insert(root, root.data);
-	        	in.insert(root, 4);
-	        	in.insert(root, 6);
-	        	in.insert(root, 5);
-	        	in.insert(root, 7);
-	        	in.insert(root, 10);
-	        	in.insert(root, 12);
-	        	in.insert(root, 9);
+				Node root = new Node(3);
+	        root = LCA.insert(root, root.data);
 
-	        assertFalse(in.find(root, 13));
-	        assertTrue(in.find(root, 12));
-	        assertEquals(root.data, 3);
+	        	LCA.insert(root, 4);
+	        	LCA.insert(root, 6);
+	        	LCA.insert(root, 5);
+	        	LCA.insert(root, 7);
+	        	LCA.insert(root, 10);
+	        	LCA.insert(root, 12);
+	        	LCA.insert(root, 9);
+
+
+	        assertTrue(LCA.find(root, 3));
+	        assertTrue(LCA.find(root, 12));
+	        assertTrue(LCA.find(root, 10));
+	        assertFalse(LCA.find(root, 20));
+	        assertFalse(LCA.find(root, 100));
+	        assertTrue(LCA.find(root, 6));
+	        assertTrue(LCA.find(root, 9));
+	        assertTrue(LCA.find(root, 7));
 	    }
 }
